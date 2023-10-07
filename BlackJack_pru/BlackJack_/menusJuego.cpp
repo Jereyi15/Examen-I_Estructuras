ee4670f7f6b762e7a4270c93bc6575124698fdb5
Jeremy Duran Vargas <107428098+Jereyi15@users.noreply.github.com>
2023-10-07 10:25:54 -0600

    Add files via upload

---
+++ BlackJack_pru/BlackJack_/menusJuego.cpp
@@ -0,0 +1,73 @@
+#include "menusJuego.h"
+/**
+menusJuego::~menusJuego() {
+
+}
+
+int menusJuego::mainMenu() {
+	int opt = 0;
+
+
+	cout << "                                  ****************************" << endl;
+	cout << "                                  * * * *BlackJack//* * * *" << endl;
+	cout << "                                  ****************************" << endl << endl << endl;
+	cout << "*******************" << endl;
+	cout << "1-Juego" << endl;
+	cout << "2-Jugadores" << endl;
+	cout << "3-Cartas" << endl;
+	cout << "4-salir" << endl;
+	cout << "*******************" << endl;
+	cin >> opt;
+
+	system("cls");
+	return opt;
+
+}
+
+void menusJuego::agregarJugadores() {
+
+	cout << "------Jugadores-------" << endl << endl;
+	cout << "*Ingrese su nombre* " << endl << endl;
+
+}
+
+
+int menusJuego::jugadores()
+{
+	int opt = 0;
+
+	cout << "*******************" << endl;
+	cout << "1-Agregar Jugador" << endl;
+	cout << "2-Ver jugadores" << endl;
+	cout << "3-Volver" << endl;
+	cout << "*******************" << endl;
+	cin >> opt;
+
+	system("cls");
+	return opt;
+}
+
+void menusJuego::porDefecto()
+{
+	cout << "-----Error: Codigo no valido" << endl << endl;
+	system("cls");
+}
+
+int menusJuego::menuCartas()
+{
+	int opt = 0;
+
+	cout << "*******************" << endl;
+	cout << "1-Crear/ver cartas" << endl;
+	cout << "2-Ver cartas" << endl;
+	cout << "3-Barajar" << endl;
+	cout << "4-Ordenar" << endl;
+	cout << "5-salir" << endl;
+	cout << "*******************" << endl;
+	cin >> opt;
+
+	system("cls");
+	return opt;
+
+}
+*/
---
+++ BlackJack_pru/BlackJack_/menusJuego.h
@@ -0,0 +1,18 @@
+#pragma once
+//clase solo para los menus
+#include <iostream>
+using namespace std;
+
+class menusJuego
+{/*
+public:
+
+	~menusJuego();
+	static int mainMenu();
+	void agregarJugadores();
+	static int jugadores();
+	static void porDefecto();
+	static int menuCartas();
+	*/
+};
+
