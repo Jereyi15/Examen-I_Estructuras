5ad2595d8a43abe7793f36f8a47ebfa0779f006c
Gabriel <gabriel.villalobos.rodriguez@est.una.ac.cr>
2023-10-06 23:18:58 -0600

    Método barajarCartas(), con su referencia.

--- BlackJack_pru/BlackJack_/ConjuntoCartas.cpp
+++ BlackJack_pru/BlackJack_/ConjuntoCartas.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <algorithm> //Librerias utilizada para barajas las cartas de manera aleatoria
 #include <random> //Librerias utilizada para barajas las cartas de manera aleatoria
+#include <queue>
 using std::stringstream;
 
 ConjuntoCartas::ConjuntoCartas(){
@@ -51,8 +52,51 @@ void ConjuntoCartas::ordenarCartas() {
         return valores.find(a.getValor()) < valores.find(b.getValor());
         });
 }
+/* Referencia del método barajarCartas():
+Techie Delight. (2023). Shuffle an array using Fisher–Yates shuffle algorithm.
+Recuoperado el 6 de octubre de 2023 de https://www.techiedelight.com/shuffle-given-array-elements-fisher-yates-shuffle/
+*/
 void ConjuntoCartas::barajarCartas() {
-   //Se debe hacer
+    int n = cartas.size();
+    queue<Carta> colaCartas;
+
+    // Si la lista no está vacía, se mete todo lo que tiene la cola
+    while (!cartas.empty()) {
+        colaCartas.push(cartas.front());
+        cartas.pop_front();
+    }
+
+    // Aleatoriedad conectada al tiempo actual, para asegurar que se baraje bien
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    vector<Carta> cartasBarajadas;
+
+    for (int indiceCarta = n; indiceCarta > 0; indiceCarta--) {
+        // Genera un índice aleatorio entre 0 e indiceCarta -1
+        int indiceAleatorio = rand() % indiceCarta;
+
+        // Mueve las cartas de la cola hasta el índice aleatorio
+        for (int i = 0; i < indiceAleatorio; i++) {
+            Carta carta = colaCartas.front();
+            colaCartas.pop();
+            colaCartas.push(carta);
+        }
+
+        // Agrega la carta en el índice aleatorio al vector de cartas barajadas
+        cartasBarajadas.push_back(colaCartas.front());
+        colaCartas.pop();
+    }
+
+    // Copia las cartas barajadas a la cola original
+    for (const Carta& carta : cartasBarajadas) {
+        colaCartas.push(carta);
+    }
+
+    // Las cartas barajadas de la cola, las manda de vuelta a la lista original
+    while (!colaCartas.empty()) {
+        cartas.push_back(colaCartas.front());
+        colaCartas.pop();
+    }
 }
 Carta ConjuntoCartas::tomarCarta() {
     if (!cartas.empty()) {
